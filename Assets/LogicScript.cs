9a833ae43c028e744f7bf2298f48dfbf03a4333f
MCGregJ <grigore.jucan10@gmail.com>
2024-09-01 21:32:20 +0300

    mare update la joc

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
    public int playerScore;
    public Text scoreText;
+   public GameObject gameOverScreen;
     
     [ContextMenu("IncreaseScore")]
-    
-    public void addScore()
+    public void addScore(int scoreToAdd)
     {
-        playerScore = playerScore + 1;
+        playerScore = playerScore + scoreToAdd;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
--- Assets/scriptulualex.cs
+++ Assets/scriptulualex.cs
@@ -1,22 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class scriptulualex : MonoBehaviour
 {
         public Rigidbody2D MyRigidbody;
         public float flapStrength;
+        public LogicScript logic;
+        public bool birdIsAlive = true;
+
     void Start()
     {
-        
+         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
              MyRigidbody.velocity = Vector2.up * flapStrength;
         }
-      
+        if ((Mouse.current.leftButton.wasPressedThisFrame) && birdIsAlive)
+         {
+            MyRigidbody.velocity = Vector2.up * flapStrength;
+         }
+         if ((Input.touchCount > 0) && birdIsAlive)
+         {
+             MyRigidbody.velocity = Vector2.up * flapStrength;
+         }
+         
     }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    logic.gameOver();
+    birdIsAlive = false;
+  }
+
 }
